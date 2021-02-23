cf6955e8093b8d5bb9c7edadbdef632b81c24d8f
Rakhman Rustamov <letayvisoko@gmail.com>
2021-02-23 20:46:45 +0300

    url launcher

--- lib/components/news_item.dart
+++ lib/components/news_item.dart
@@ -3,6 +3,7 @@ import 'package:nt_top_news/models/article_abstract.dart';
 import 'package:nt_top_news/theme.dart';
 import 'package:nt_top_news/utils/date_parser.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class NewsItem extends StatelessWidget {
 
@@ -18,7 +19,6 @@ class NewsItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    double _w = MediaQuery.of(context).size.width;
     DateTime _now = DateTime.now();
 
     return Container(
@@ -28,72 +28,86 @@ class NewsItem extends StatelessWidget {
         color: Colors.white,
         boxShadow: kStandartBoxShadow
       ),
-      child: Container(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            if (article.coverUrl != null)
-            Center(
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(15),
-                child: CachedNetworkImage(
-                  imageUrl: article.coverUrl,
-                  height: 250,
-                  fit: BoxFit.cover,
-                  placeholder: (context, url) => Container(
-                    color: Colors.white,
+      child: InkWell(
+        borderRadius: BorderRadius.circular(15),
+        onTap: () {
+          _launchURL(article.shortUrl);
+        },
+        child: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              if (article.coverUrl != null)
+              Center(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: CachedNetworkImage(
+                    imageUrl: article.coverUrl,
+                    height: 250,
+                    fit: BoxFit.cover,
+                    placeholder: (context, url) => Container(
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ),
-            ),
-            if (article.title != null)
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 15),
-                    child: Text(
-                      article.title,
-                      style: TextStyle(
-                        fontWeight: FontWeight.w600,
+              if (article.title != null)
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 15),
+                      child: Text(
+                        article.title,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                        ),
                       ),
                     ),
-                  ),
-                  Text(
-                    article.abstract,
-                    style: TextStyle(color: Colors.grey),
-                  ),
-                  SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        article.section,
-                        style: TextStyle(
-                          color: Colors.blue,
-                          fontSize: 14,
-                          fontWeight: FontWeight.w600
+                    Text(
+                      article.abstract,
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          article.section,
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600
+                          ),
                         ),
-                      ),
-                      Text(
-                        DateParser.parseDate(
-                          article.updatedDate,
-                          _now
-                        ),
-                        style: TextStyle(
-                          color: Colors.grey
-                        ),
-                      )
-                    ],
-                  ),
-                ],
+                        Text(
+                          DateParser.parseDate(
+                            article.updatedDate,
+                            _now
+                          ),
+                          style: TextStyle(
+                            color: Colors.grey
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
+  }
+}
+
+_launchURL(String url) async {
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
   }
 }
